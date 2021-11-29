fa525fe9c064668e4f94d548ba893dc56c21687e
nplimbani <niravlimbani189@gmail.com>
2021-11-28 21:27:00 -0600

    Splash screen added

---
+++ app/src/main/java/com/groupB/foodoasis/Activity/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.groupB.foodoasis.Activity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.groupB.foodoasis.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        TextView appName;
+//        LottieAnimationView lottieAnimationView;
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        appName = (TextView) findViewById(R.id.txt_appname);
+
+        Typeface typeface = ResourcesCompat.getFont(
+                this,
+                R.font.tusj);
+        appName.setTypeface(typeface);
+
+//        appName.animate().translationY(2000).setDuration(1000).setStartDelay(3000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 3000);
+    }
+}
